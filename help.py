8a38cac1afba8f666c444036f0d62760b9f679eb
aidanMellin <aidan.mellin@gmail.com>
2021-02-06 11:36:37 -0500

    Fixed an issue with help (indentation problem)

--- help.py
+++ help.py
@@ -5,18 +5,18 @@ def help_main(help_args):
 		resp = "Usage:\n**~help [monke/joker/horny/todo]**"
 	else: #Help call containing arg
 		call = help_args[0]
-	if "monke" in call: #Open monke.txt
-		with open('help/monke.md','r') as fp:
-			resp = f"{fp.read()}".format(**locals())
-	elif "joker" in call: #Open joker.txt
-		with open('help/joker.md','r') as fp:
-			resp = f"{fp.read()}".format(**locals())
-	elif "horny" in call: #Open horny.txt
-		with open('help/horny.md','r') as fp:
-			resp = f"{fp.read()}".format(**locals())
-	elif "todo" in call: #Open todo.txt
-		with open('help/todo.md','r') as fp:
-			resp = f"{fp.read()}".format(**locals())
-	else:
-		resp = "invalid call"
+        if "monke" in call: #Open monke.txt
+            with open('help/monke.md','r') as fp:
+                resp = f"{fp.read()}".format(**locals())
+        elif "joker" in call: #Open joker.txt
+            with open('help/joker.md','r') as fp:
+                resp = f"{fp.read()}".format(**locals())
+        elif "horny" in call: #Open horny.txt
+            with open('help/horny.md','r') as fp:
+                resp = f"{fp.read()}".format(**locals())
+        elif "todo" in call: #Open todo.txt
+            with open('help/todo.md','r') as fp:
+                resp = f"{fp.read()}".format(**locals())
+        else:
+            resp = "invalid call"
 	return resp
